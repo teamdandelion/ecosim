9a245273348f43215eb5183b7dcfbe5562216f44
Brian Holder-Chow Lin On <brian.holderchow@gmail.com>
2013-08-10 03:34:10 -0700

    add resource collection

---
+++ src/main/java/ResourceCollection.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Collection;
+
+class ResourceCollection {
+    // n resources
+    private Collection<Long> resources;
+    private static final int N_RESOURCE_TYPES = 0;
+    public ResourceCollection() {
+        this.resources = new ArrayList<Long>();
+        final Long INITIAL_VAL = 0L;
+        for	(int i = 0; i < N_RESOURCE_TYPES; ++i) {
+        	this.resources.add(INITIAL_VAL);
+        }
+    }
+}
+
