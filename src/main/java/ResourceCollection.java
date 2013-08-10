c2e058a6b7c5cbf8687b68727f706a81505a1733
Brian Holder-Chow Lin On <brianholderchow@users.noreply.github.com>
2013-08-10 05:31:12 -0700

    Update ResourceCollection.java

--- src/main/java/ResourceCollection.java
+++ src/main/java/ResourceCollection.java
@@ -2,6 +2,6 @@ class ResourceCollection implements Resources {
     private static final int N_RESOURCE_TYPES = 10;
     private int[] resources;
     public ResourceCollection() {
-        this.resources = new int[N_RESOURCE_TYPES];
+        this.resources = new int[N_RESOURCE_TYPES]; // initialized to 0
     }
 }
